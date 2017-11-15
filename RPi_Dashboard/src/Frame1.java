30018c5573891b4dfbed6477bf21ca5a5c9b1058
sbaydush <sbaydush@Pandora>
2017-11-15 11:58:43 -0500

    Changed format from table to imitate Coinbase website for easier viewing.

--- RPi_Dashboard/src/Frame1.java
+++ RPi_Dashboard/src/Frame1.java
@@ -2,6 +2,8 @@ import java.awt.EventQueue;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -10,19 +12,23 @@ import javax.swing.JFrame;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.awt.BorderLayout;
-import javax.swing.JTable;
-import javax.swing.border.LineBorder;
 import java.awt.Color;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.JScrollPane;
 import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 
 public class Frame1 {
 
 	private JFrame frmRpiDashboard;
-	private JTable tblCrypto;
+	private JLabel lblPrice;
+	private JLabel lblSign;
+	private JLabel lblChange;
+	private JLabel lblPriceBTC;
+	private JLabel lblPriceETH;
+	private JLabel lblPriceLTC;
+	private JLabel lblCryptoBG;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -63,9 +69,9 @@ public class Frame1 {
 	    	try {
 	    	    JSONObject json = new JSONObject(readUrl("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,LTC,ETH&tsyms=USD&e=Coinbase&extraParams=your_app_name"));
 
+	    	    //Set numberformat for String so it will have commas (e.x. 7,230.35)
+	    	    NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.US);
 	    	    
-	    	    ImageIcon downarrow = new ImageIcon("resources/down_red_arrow.png");
-	    	    ImageIcon uparrow = new ImageIcon("resources/up_green_arrow.png");
 	    	    
 	    	    //Get Current Prices
 	    	    Double BTC_Price = json.getJSONObject("RAW").getJSONObject("BTC").getJSONObject("USD").getDouble("PRICE");
@@ -73,35 +79,66 @@ public class Frame1 {
 	    	    Double LTC_Price = json.getJSONObject("RAW").getJSONObject("LTC").getJSONObject("USD").getDouble("PRICE");
 
 	    	    //Get 24Hour High Prices
-	    	    Double BTC_High = json.getJSONObject("RAW").getJSONObject("BTC").getJSONObject("USD").getDouble("HIGH24HOUR");
-	    	    Double ETH_High = json.getJSONObject("RAW").getJSONObject("ETH").getJSONObject("USD").getDouble("HIGH24HOUR");
-	    	    Double LTC_High = json.getJSONObject("RAW").getJSONObject("LTC").getJSONObject("USD").getDouble("HIGH24HOUR");
+	    	    //Double BTC_High = json.getJSONObject("RAW").getJSONObject("BTC").getJSONObject("USD").getDouble("HIGH24HOUR");
+	    	    //Double ETH_High = json.getJSONObject("RAW").getJSONObject("ETH").getJSONObject("USD").getDouble("HIGH24HOUR");
+	    	    //Double LTC_High = json.getJSONObject("RAW").getJSONObject("LTC").getJSONObject("USD").getDouble("HIGH24HOUR");
 	    	    
 	    	    //Get 24Hour Low Prices
-	    	    Double BTC_Low = json.getJSONObject("RAW").getJSONObject("BTC").getJSONObject("USD").getDouble("LOW24HOUR");
-	    	    Double ETH_Low = json.getJSONObject("RAW").getJSONObject("ETH").getJSONObject("USD").getDouble("LOW24HOUR");
-	    	    Double LTC_Low = json.getJSONObject("RAW").getJSONObject("LTC").getJSONObject("USD").getDouble("LOW24HOUR");
+	    	    //Double BTC_Low = json.getJSONObject("RAW").getJSONObject("BTC").getJSONObject("USD").getDouble("LOW24HOUR");
+	    	    //Double ETH_Low = json.getJSONObject("RAW").getJSONObject("ETH").getJSONObject("USD").getDouble("LOW24HOUR");
+	    	    //Double LTC_Low = json.getJSONObject("RAW").getJSONObject("LTC").getJSONObject("USD").getDouble("LOW24HOUR");
 	    	    
+	    	    //Get 24Hour Change in Price
+	    	    Double BTC_Change = json.getJSONObject("RAW").getJSONObject("BTC").getJSONObject("USD").getDouble("CHANGE24HOUR");
+	    	    Double ETH_Change = json.getJSONObject("RAW").getJSONObject("ETH").getJSONObject("USD").getDouble("CHANGE24HOUR");
+	    	    Double LTC_Change = json.getJSONObject("RAW").getJSONObject("LTC").getJSONObject("USD").getDouble("CHANGE24HOUR");
+	    	    
+	    	    //Round 24Hour Change
+	    	    
+	    	    BTC_Change = Math.round(BTC_Change * 100.0) / 100.0;
+	    	    ETH_Change = Math.round(ETH_Change * 100.0) / 100.0;
+	    	    LTC_Change = Math.round(LTC_Change * 100.0) / 100.0;
+	    	    
+	    	    
+	    	    
+	    	    
+	    	    
+	    	    //Populate Coin Labels
+	    	    //tblCrypto.setValueAt("BTC", 0, 0);
+	    	    //tblCrypto.setValueAt("LTC", 1, 0);
+	    	    //tblCrypto.setValueAt("ETH", 2, 0);
 	    	    
 	    	    //Populate Current Prices
-	    	    tblCrypto.setValueAt("$"+BTC_Price, 0,2);
-	    	    tblCrypto.setValueAt("$"+LTC_Price, 1,2);
-	    	    tblCrypto.setValueAt("$"+ETH_Price, 2,2);
+	    	    String strBTCPrice = numberFormat.format(BTC_Price);
+	    	    lblPrice.setText(strBTCPrice);
+	    	    lblPriceBTC.setText(strBTCPrice);
 	    	    
-	    	    //Add increase/decrease Arrows
-	    	    tblCrypto.setValueAt(downarrow, 0,1);
-	    	    tblCrypto.setValueAt(uparrow, 1,1);
-	    	    tblCrypto.setValueAt("a", 2,1);
+	    	    String strETHPrice = numberFormat.format(ETH_Price);
+	    	    lblPriceETH.setText(strETHPrice);
 	    	    
-	    	    //Populate High Prices
-	    	    tblCrypto.setValueAt("$"+BTC_High, 0,3);
-	    	    tblCrypto.setValueAt("$"+LTC_High, 1,3);
-	    	    tblCrypto.setValueAt("$"+ETH_High, 2,3);
+	    	    String strLTCPrice = numberFormat.format(LTC_Price);
+	    	    lblPriceLTC.setText(strLTCPrice);
 	    	    
-	    	    //Populate Low Prices
-	    	    tblCrypto.setValueAt("$"+BTC_Low, 0,4);
-	    	    tblCrypto.setValueAt("$"+LTC_Low, 1,4);
-	    	    tblCrypto.setValueAt("$"+ETH_Low, 2,4);
+	    	    
+	    	    //Populate 24 Hour Change in Price
+	    	    String strBTCChange = (numberFormat.format(BTC_Change)).replaceAll("-", "");
+	    	    lblChange.setText(strBTCChange);
+	    	    
+	    	    
+	    	    
+	    	    //Set Change Sign based on Positive or negative value
+	    	    if(BTC_Change < 0)
+	    	    {
+	    	    	lblSign.setFont(new Font("Cantarell", Font.BOLD, 24));
+	    	    	lblSign.setForeground(new Color(204, 0, 0));
+	    	    	lblSign.setText("-");
+	    	    }else
+	    	    {
+	    	    	lblSign.setFont(new Font("Cantarell", Font.BOLD, 19));
+	    	    	lblSign.setForeground(new Color(0, 255, 0));
+	    	    	lblSign.setText("+");
+	    	    }
+	    	    
 	    	    
 	    	    
 	    	    
@@ -125,7 +162,7 @@ public class Frame1 {
 		initialize();
 		
 		Timer timer = new Timer();
-		timer.schedule(new GetCryptoPrices(), 0, 15000);
+		timer.schedule(new GetCryptoPrices(), 0, 10000);
 	}
 
 	/**
@@ -135,41 +172,52 @@ public class Frame1 {
 		frmRpiDashboard = new JFrame();
 		frmRpiDashboard.setAlwaysOnTop(true);
 		frmRpiDashboard.setTitle("RPi Dashboard");
-		frmRpiDashboard.setBounds(100, 100, 450, 300);
+		frmRpiDashboard.setBounds(0, 0, 480, 320);
 		frmRpiDashboard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ImageIcon icon = new ImageIcon("resources/Background.png");
+		frmRpiDashboard.getContentPane().setLayout(null);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		frmRpiDashboard.getContentPane().add(scrollPane, BorderLayout.EAST);
+		lblPrice = new JLabel("0.00");
+		lblPrice.setFont(new Font("Dialog", Font.BOLD, 19));
+		lblPrice.setBounds(76, 65, 94, 23);
+		frmRpiDashboard.getContentPane().add(lblPrice);
 		
-		tblCrypto = new JTable();
-		tblCrypto.setFont(new Font("Dialog", Font.PLAIN, 18));
-		scrollPane.setViewportView(tblCrypto);
-		tblCrypto.setShowHorizontalLines(false);
-		tblCrypto.setShowVerticalLines(false);
-		tblCrypto.setShowGrid(false);
-		tblCrypto.setRowSelectionAllowed(false);
-		tblCrypto.setModel(new DefaultTableModel(
-			new Object[][] {
-				{"BTC", null, null, null, null},
-				{"LTC", null, null, null, null},
-				{"ETH", null, null, null, null},
-			},
-			new String[] {
-				"Coin", "", "Price", "High", "Low"
-			}
-		));
+		lblSign = new JLabel("");
+		lblSign.setForeground(new Color(0, 255, 0));
+		lblSign.setFont(new Font("Cantarell", Font.BOLD, 19));
+		lblSign.setBounds(288, 69, 66, 15);
+		frmRpiDashboard.getContentPane().add(lblSign);
 		
-		tblCrypto.getColumnModel().getColumn(0).setPreferredWidth(51);
-		tblCrypto.getColumnModel().getColumn(1).setPreferredWidth(25);
-		tblCrypto.getColumnModel().getColumn(2).setPreferredWidth(116);
-		tblCrypto.getColumnModel().getColumn(3).setPreferredWidth(113);
-		tblCrypto.getColumnModel().getColumn(4).setPreferredWidth(134);
-		tblCrypto.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblChange = new JLabel("0.00");
+		lblChange.setFont(new Font("Dialog", Font.BOLD, 19));
+		lblChange.setBounds(306, 57, 94, 38);
+		frmRpiDashboard.getContentPane().add(lblChange);
+		
+		lblPriceBTC = new JLabel("0.00");
+		lblPriceBTC.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblPriceBTC.setBounds(63, 9, 66, 15);
+		frmRpiDashboard.getContentPane().add(lblPriceBTC);
+		
+		lblPriceETH = new JLabel("0.00");
+		lblPriceETH.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblPriceETH.setBounds(182, 9, 66, 15);
+		frmRpiDashboard.getContentPane().add(lblPriceETH);
+		
+		lblPriceLTC = new JLabel("0.00");
+		lblPriceLTC.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblPriceLTC.setBounds(283, 9, 66, 15);
+		frmRpiDashboard.getContentPane().add(lblPriceLTC);
+		
+		lblCryptoBG = new JLabel();
+		lblCryptoBG.setVerticalAlignment(SwingConstants.TOP);
+		lblCryptoBG.setBounds(0, 0, 480, 151);
+		lblCryptoBG.setIcon(icon);
+		frmRpiDashboard.getContentPane().add(lblCryptoBG);
+
 		
 		
 		
 		//frame.getContentPane().add(tblCrypto, BorderLayout.SOUTH);
 	}
-
 }
 
